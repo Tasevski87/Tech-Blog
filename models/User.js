9499d0ee16c7d0f0754adfb2fe095247c5d258a7
toshetasevski <toshetasevski@Toshes-MacBook-Pro.local>
2022-02-25 13:31:41 -0500

    bug

---
+++ models/User.js
@@ -0,0 +1,63 @@
+// import important parts of sequelize library
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+const bcrypt = require('bcrypt')
+
+// Initialize model (table) by extending off Sequelize's Model class
+class User extends Model {
+    checkPassword(loginPwd) {
+        return bcrypt.compareSync(loginPwd, this.password)
+    }
+}
+
+// set up fields and rules for model
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            },
+        },
+
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4],
+            },
+        },
+    },
+    {
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            async beforeUpdate(updateUserData) {
+                updateUserData.password = await bcrypt.hash(updateUserData.password, 10)
+                return updateUserData;
+            },
+        },
+        sequelize: sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "user",
+    }
+);
+
+module.exports = User;
+
